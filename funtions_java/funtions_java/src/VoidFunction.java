4c8e5dd594670abad331371a965b69f68628dfee
Andres Daza <felipedazasernadim@gmail.com>
2025-05-13 20:13:46 -0500

    feat: función sin parámetros y sin retorno implementada

---
+++ funtions_java/funtions_java/src/VoidFunction.java
@@ -0,0 +1,8 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        saludar();
+    }
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
