970181424e7f39fc57ef98b30668cb7eae54e81a
Andres Daza <felipedazasernadim@gmail.com>
2025-05-13 20:20:18 -0500

    feat: función con parámetros y sin retorno implementada

---
+++ funtions_java/funtions_java/src/PrintSum.java
@@ -0,0 +1,9 @@
+public class PrintSum {
+    public static void main(String[] args) {
+        mostrar_suma(10, 23);
+    }
+    public static void mostrar_suma(int a, int b){
+        int suma = a + b;
+        System.out.println("Lasuma es: " + suma);
+    }
+}
