742ce33fbfbd69df4d9fb6e9e00a0682aa2e3d15
Andres Daza <felipedazasernadim@gmail.com>
2025-05-13 20:24:25 -0500

    feat: función sin parámetros y con retorno implementada

---
+++ funtions_java/funtions_java/src/RandomNumber.java
@@ -0,0 +1,9 @@
+public class RandomNumber {
+    public static void main(String[] args) {
+        int numero = obtener_numero();
+        System.out.println("Número devuelto: " + numero);
+    }
+    public static int obtener_numero(){
+        return 7;
+    }
+}
